3b00e70519107337d43723942ca3b082e967861c
gouthamipeddiraju <gouthamipeddiraju@gmail.com>
2021-01-27 19:46:12 +1100

    profile added

---
+++ stg/charts/precache-manifest.80ba873ca9796d49f1193c97d2ecbb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c2709f98ea4cbf476867224a09dada",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0416386518456d67069c",
+    "url": "./static/css/main.c9a471fb.chunk.css"
+  },
+  {
+    "revision": "adee64a803d34b88e02c",
+    "url": "./static/js/2.4f31adda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4f31adda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0416386518456d67069c",
+    "url": "./static/js/main.aa02bb94.chunk.js"
+  },
+  {
+    "revision": "c5db55c81e0f3be99a8b",
+    "url": "./static/js/runtime-main.b50dda40.js"
+  }
+]);
---
+++ stg/charts/precache-manifest.a0c348dd67def60f9ad057e86b45cf73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7310762183e2adab8005d15464197618",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b5ae8e1cc3c44f56980",
+    "url": "./static/css/main.3061c280.chunk.css"
+  },
+  {
+    "revision": "adee64a803d34b88e02c",
+    "url": "./static/js/2.4f31adda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4f31adda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5ae8e1cc3c44f56980",
+    "url": "./static/js/main.ffc21c4c.chunk.js"
+  },
+  {
+    "revision": "c5db55c81e0f3be99a8b",
+    "url": "./static/js/runtime-main.b50dda40.js"
+  }
+]);
